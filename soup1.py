7c0944af790dfcd38a6d23efc37d498e6eca9bf5
cowjd <bcj7231@cu.ac.kr>
2022-04-14 10:26:06 +0900

    20220414

--- soup1.py
+++ soup1.py
@@ -1,7 +1,7 @@
 from bs4 import BeautifulSoup
 htmlString = '''<html>
 <head><title>My Document</title></head>
-<body>Main text.</body></html
+<body>Main text.</body></html>
 '''
 
 soup = BeautifulSoup(htmlString, "html.parser")
